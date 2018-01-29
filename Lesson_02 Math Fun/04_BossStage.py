9c54f2cfda59717e9b0358452330c9c45d29b0bc
James Dixon <jamesdixon117@gmail.com>
2018-01-29 20:17:29 +1100

    Syncing up

--- Lesson_02 Math Fun/04_BossStage.py
+++ Lesson_02 Math Fun/04_BossStage.py
@@ -1,14 +1,17 @@
 def main():
 	# Your code goes here:
 	
-	print("This program can divide two numbers together")
-	# To get user input you use the function "input("Question")"
-	number1 = input("Whats your first number? ")
-	number2 = input("Whats your second number? ")
+	firstname = input("What is your first name? ")
+	lastname = input("What is your last name? ")
+	age = input("What is your age? ")
+	haircolour = input("What is your hair colour? ")
+	shoes = input("What is your brand of shoes? ")
+	eyecolour = input("What is your eye colour? ")
+	dogsname = input("What is your dogs name? ")
 	
-	answer = float(number1) / float(number2)
-	print("The answer is: " + str(answer))
-	
+	print("My name is " + str(firstname) + " " + str(lastname) + " and I am " + str(age) + " years old.")
+	print("I have " + str(haircolour) + " hair and have " + str(eyecolour) + " eyes.")
+	print("I have " + str(shoes) + " shoes and my dogs name is " + str(dogsname))
 	
 	return 0
 	
