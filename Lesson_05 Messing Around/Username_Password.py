95f2ae258de25ca25433480d1a01b16e5638c102
James Dixon <jamesdixon117@gmail.com>
2018-02-19 19:41:25 +1100

    Syncing up

--- Lesson_05 Messing Around/Username_Password.py
+++ Lesson_05 Messing Around/Username_Password.py
@@ -1,10 +1,36 @@
 
+# I am a global variable. 
+
+# These usernames are going to be Global Variables. 
+# They are the ones we are comparing to these passwords. 
+USERNAME = "Jadboy20"
+PASSWORD = "SillyMan"
+
 def main():
 	# THis program I am going to ask the user to input their password and username.
 	# They must get BOTH correct to go on. 
+	
+	input_username = input("Enter your username: ")
+	input_password = input("Enter your password: ")
+	
+	if str(input_username) == USERNAME:
+		print("User name is correct!!")
+		if str(input_password) == PASSWORD:
+			print("Password is correct!!")
+			print("Welcome Jadboy20!")
+		else:
+			print("Password is incorrect!")
+	else:
+		print("User name does not exist!")
+	
+	input("Press enter to exit...")
 
+
+	
+	
 if __name__ == "__main__":
 	import sys
 	main()
 	sys.exit()
+	print(variable)
 	
